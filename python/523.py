56ab527b7c94210a1bb0b36b84f27df5d9ac26df
Stephen Macke <smacke@cs.stanford.edu>
2016-07-15 00:28:33 -0700

    523

---
+++ python/523.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+def memoize(f):
+    _f = {}
+    def f_mem(*args):
+        if args in _f:
+            return _f[args]
+        ans = f(*args)
+        _f[args] = ans
+        return ans
+    return f_mem
+
+@memoize
+def s(n):
+    if n==0:
+        return 0.
+    return t(n) + s(n-1)
+
+# probably easy to get an explicit form for this
+# but why bother; it's linear
+# represents # of steps needed to get nth smallest
+# item into correct position starting from 0th position
+# that is, # steps for:
+# [n, 0, 1, 2, ..., n-1] ==> [0, 1, 2, ..., n]
+
+@memoize
+def t(n):
+    if n==0:
+        return 0.
+    if n==1:
+        return 1.
+    return n + s(n-1)
+
+# with probability 1/n, kth smallest item goes in last position
+# then we need e(n-1) moves in expectation to sort left side of
+# list; then another 1+t(k) moves to put rightmost element into
+# correct position. the reason we can use e(n-1) is because we
+# only care about the rank of items, and the distribution of ranks
+# given the rightmost element is still uniform.
+
+@memoize
+def e(n):
+    if n==0:
+        return 0.
+    ans = 0.
+    for k in xrange(n-1):
+        ans += (e(n-1) + 1. + t(k)) / n
+    return ans + e(n-1) / n
+
+print '%.2f' % e(30)
